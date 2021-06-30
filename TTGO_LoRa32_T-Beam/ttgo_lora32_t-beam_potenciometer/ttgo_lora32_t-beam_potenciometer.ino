0ab4e4e86c3b4e15496db4dff713ee3185cbec4e
HenryCodeT <120686@unsaac.edu.pe>
2021-06-29 21:33:19 -0500

    anadiendo ttgo lora32 t-beam

---
+++ TTGO_LoRa32_T-Beam/ttgo_lora32_t-beam_potenciometer/ttgo_lora32_t-beam_potenciometer.ino
@@ -0,0 +1,16 @@
+
+#define BATTERY_PIN 35
+
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+}
+ 
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(BATTERY_PIN);
+  // print out the value you read:
+  Serial.println(sensorValue);
+  delay(1);        // delay in between reads for stability
+}
